59705571398f22fee75345a0b8d9660940b42143
Julian Patterson <julian.patterson@gmail.com>
2023-03-16 19:37:28 -0400

    The create.sql, insert.sql, and sql_queries.py contain SQL code that passes 14 SQL CRUD and query tests.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Cuddly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "F", "Blonde", "Hyper", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "Black", "Sassy", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sargeant Brown", 2, "M", "Brown", "Aggressive", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "White", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "M", "Green", "Gentle", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "F", "Blonde", "Subdued", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, "M", "Brown", "Angry", True);
